c30ce259d693daefc6915ce41ff7ece640478ad8
DonnyGru <91632259+DonnyGru@users.noreply.github.com>
2021-11-25 17:07:36 +0100

    Add files via upload

---
+++ java_pub/java_pub.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = bob;
+
+if (age >= 18){
+    console.log("You are allowed in!");
+} else{
+    console.log("You are too young!");
+}
+
+if (isFemale){
+    console.log("You are a woman! Come in!");
+} else {
+    console.log("Sorry, only woman are allowed to go in!");
+}
+
+if (driverStatus == bob) {
+    console.log("You can drive!");
+} else {
+    console.log("You cannot drive!");
+}
